11a2c189ec948c51ccaadddcb0c5c95f8c95fe21
Karl Bonde Torp <kart@itu.dk>
2021-09-27 17:54:53 +0200

    Completed Extensions

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -6,9 +6,34 @@ namespace BDSA2020.Assignment02.Tests
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
+        public void IsSecure_given_https_returns_true()
         {
+            var input = new Uri("https://google.dk");
+            Assert.True(input.IsSecure());
 
         }
+        [Fact]
+        public void IsSecure_given_http_returns_false()
+        {
+            var input = new Uri("http://google.dk");
+            Assert.False(input.IsSecure());
+        }
+        [Fact]
+        public void WordCount_given_hej_med_dig_returns_3()
+        {
+            var input = "hej med dig";
+            Assert.Equal(3, input.WordCount());
+
+        }
+        [Fact]
+        public void WordCount_given_hej_returns_1()
+        {
+            var input = "hej";
+            Assert.Equal(1, input.WordCount());
+
+        }
+
+
+
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,20 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+
+        public static bool IsSecure(this Uri uri)
+        {
+            return uri.Scheme == "https";
+        }
+        public static int WordCount(this string sentence)
+        {
+            Regex exp = new Regex("(\\p{L}\\p{M}*)+");
+            return exp.Matches(sentence).Count;
+        }
+
     }
 }
