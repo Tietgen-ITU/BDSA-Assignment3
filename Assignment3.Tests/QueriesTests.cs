a75e355759c8fe6c693eae35e532250cfe7302a8
Karl Bonde Torp <kart@itu.dk>
2021-09-30 18:30:50 +0200

    Completed Final exercises:w

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BDSA2020.Assignment02.Tests
@@ -6,9 +7,71 @@ namespace BDSA2020.Assignment02.Tests
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void GetRowlingsWizards_returns_Hermione_Harry_Voldemort_Albus_Severus_Harry()
         {
-
+            string[] expected = { "Hermione Granger", "Harry Potter", "Voldemort", "Albus Dumbledore", "Severus Snape", "Harry Potter" };
+            var actual = Queries.GetRowlingsWizards();
+            Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void GetRowlingsWizardsLinq_returns_Hermione_Harry_Voldemort_Albus_Severus_Harry()
+        {
+            string[] expected = { "Hermione Granger", "Harry Potter", "Voldemort", "Albus Dumbledore", "Severus Snape", "Harry Potter" };
+            var actual = Queries.GetRowlingsWizardsLinq();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetSithYear_returns_1977()
+        {
+            var expected = 1977;
+            var actual = Queries.GetSithYear();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetSithYearLinq_returns_1977()
+        {
+            var expected = 1977;
+            var actual = Queries.GetSithYearLinq();
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void GetUniqueHarryWizards_returns_Hermione_Harry_Voldemort_Albus_Severus_And_1997()
+        {
+            var expected = new List<(string, int?)> { ("Hermione Granger", 1997), ("Harry Potter", 1997), ("Voldemort", 1997), ("Albus Dumbledore", 1997), ("Severus Snape", 1997) };
+            var actual = Queries.GetUniqueHarryWizards();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetUniqueHarryWizardsLinq_returns_Hermione_Harry_Voldemort_Albus_Severus_And_1997()
+        {
+            var expected = new List<(string, int?)> { ("Hermione Granger", 1997), ("Harry Potter", 1997), ("Voldemort", 1997), ("Albus Dumbledore", 1997), ("Severus Snape", 1997) };
+            var actual = Queries.GetUniqueHarryWizardsLinq();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetWizardNamesInReverse_returns_Hermione_Harry_Voldemort_Albus_Severus_And_1997()
+        {
+            string[] expected = { "Gandalf The Gray", "Sauron", "Albus Dumbledore", "Harry Potter", "Harry Potter", "Hermione Granger", "Severus Snape", "Voldemort", "Darth Vader", "Digory Kirke", "Polly Plummer" };
+            var actual = Queries.GetWizardNamesInReverse();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetWizardNamesInReverseLinq_returns_Hermione_Harry_Voldemort_Albus_Severus_And_1997()
+        {
+            string[] expected = { "Gandalf The Gray", "Sauron", "Albus Dumbledore", "Harry Potter", "Harry Potter", "Hermione Granger", "Severus Snape", "Voldemort", "Darth Vader", "Digory Kirke", "Polly Plummer" };
+            var actual = Queries.GetWizardNamesInReverseLinq();
+            Assert.Equal(expected, actual);
+        }
+
+
+
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,72 @@
+using System.Linq;
+using System;
+using System.Collections.Generic;
 namespace BDSA2020.Assignment02
 {
     public class Queries
     {
+        public static IEnumerable<string> GetRowlingsWizards()
+        {
+            return Wizard.Wizards.Value.Where(x => x.Creator == "J.K. Rowling").Select(x => x.Name);
+        }
+
+        public static IEnumerable<string> GetRowlingsWizardsLinq()
+        {
+            return from x in Wizard.Wizards.Value where x.Creator == "J.K. Rowling" select x.Name;
+        }
+
+        public static int GetSithYear()
+        {
+            return Wizard.Wizards.Value.Where(x => x.Name.Contains("Darth")).OrderBy(x => x.Year).Select(x => x.Year).First().Value;
+        }
+
+        public static int GetSithYearLinq()
+        {
+            return (from x in Wizard.Wizards.Value where x.Name.Contains("Darth") orderby x.Year select x.Year).First().Value;
+        }
+
+        public static IEnumerable<(string, int?)> GetUniqueHarryWizards()
+        {
+            return Wizard.Wizards.Value.Where(x => x.Medium.Contains("Harry Potter")).Distinct(new WizardComparer()).Select(x => (x.Name, x.Year));
+        }
+
+        public static IEnumerable<(string, int?)> GetUniqueHarryWizardsLinq()
+        {
+            return (from x in Wizard.Wizards.Value where x.Medium.Contains("Harry Potter") select x).Distinct(new WizardComparer()).Select(x => (x.Name, x.Year));
+        }
+
+
+        public static IEnumerable<string> GetWizardNamesInReverse()
+        {
+            return Wizard.Wizards.Value.OrderBy(x => x.Name).OrderByDescending(x => x.Creator).Select(x => x.Name);
+        }
+
+        public static IEnumerable<string> GetWizardNamesInReverseLinq()
+        {
+            return from x in Wizard.Wizards.Value orderby x.Name orderby x.Creator descending select x.Name;
+        }
+
     }
+
+    class WizardComparer : IEqualityComparer<Wizard>
+    {
+        public bool Equals(Wizard w1, Wizard w2)
+        {
+            if (w2 == null && w1 == null)
+                return true;
+            else if (w1 == null || w2 == null)
+                return false;
+            else if (w1.Name == w2.Name)
+                return true;
+            else
+                return false;
+        }
+        public int GetHashCode(Wizard w)
+        {
+            return w.Name.GetHashCode();
+        }
+
+    }
+
+
 }
